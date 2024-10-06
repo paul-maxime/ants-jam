47274d1d376c0041a6339b3a4162bb87505f93b2
Paul-Maxime <github@paulmaxime.fr>
2024-10-06 18:38:09 +0200

    Just a floor with ambiant light and sun

--- Scripts/GameBehavior.gd
+++ Scripts/GameBehavior.gd
@@ -3,5 +3,6 @@ extends Node
 var ant_scene = preload("res://Scenes/Entities/Ant.tscn")
 
 func _ready() -> void:
-	for i in range(2000):
-		add_child(ant_scene.instantiate())
+	pass
+	#for i in range(2000):
+	#	add_child(ant_scene.instantiate())
